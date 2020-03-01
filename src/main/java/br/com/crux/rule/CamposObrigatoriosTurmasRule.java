c55ce482870f6b3f1b24665eb8a1f0bc440da936
Wilson Souza <wilsons80@gmail.com>
2020-03-01 10:11:29 -0300

    Permite valor nula na data de previsão inicio da Turma

--- src/main/java/br/com/crux/rule/CamposObrigatoriosTurmasRule.java
+++ src/main/java/br/com/crux/rule/CamposObrigatoriosTurmasRule.java
@@ -16,10 +16,6 @@ public class CamposObrigatoriosTurmasRule {
 			throw new CamposObrigatoriosException("A descrição deve ser informada.");
 		}
 
-		if (Objects.isNull(to.getDataPrevisaoInicio())) {
-			throw new CamposObrigatoriosException("Data de Início de Previsão deve ser informada.");
-		}
-
 		if (Objects.isNull(to.getUnidade())) {
 			throw new CamposObrigatoriosException("Unidade deve ser informada.");
 		}
