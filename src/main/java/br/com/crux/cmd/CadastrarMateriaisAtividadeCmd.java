d969655a14c8ca3c47ece02a41148fe0db54f53c
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-07-09 11:27:57 -0300

    Valida o cadastro de oficinas sem informar materiais

--- src/main/java/br/com/crux/cmd/CadastrarMateriaisAtividadeCmd.java
+++ src/main/java/br/com/crux/cmd/CadastrarMateriaisAtividadeCmd.java
@@ -1,15 +1,12 @@
 package br.com.crux.cmd;
 
 import java.util.List;
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.crux.builder.MateriaisAtividadeTOBuilder;
 import br.com.crux.dao.repository.MateriaisAtividadeRepository;
 import br.com.crux.entity.MateriaisAtividade;
-import br.com.crux.exception.NotFoundException;
 import br.com.crux.rule.CamposObrigatoriosMateriaisAtividadeRule;
 import br.com.crux.to.MateriaisAtividadeTO;
 
@@ -33,9 +30,6 @@ public class CadastrarMateriaisAtividadeCmd {
 	
 
 	public void cadastrarAll(List<MateriaisAtividadeTO> materiaisAtividade, Long idAtividade) {
-		if(Objects.nonNull(materiaisAtividade) || materiaisAtividade.isEmpty()) {
-			throw new NotFoundException("Adicione pelo menos um material.");
-		}
 		materiaisAtividade.forEach(ca -> {ca.setIdAtividade(idAtividade); cadastrar(ca); });
 	}
 }
