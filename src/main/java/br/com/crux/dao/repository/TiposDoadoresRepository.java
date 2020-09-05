bbc8d68ef4b0288e3152d7dc067b3452517e8bd1
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-09-05 13:42:45 -0300

    Implementação do doador na tela de movimentação

--- src/main/java/br/com/crux/dao/repository/TiposDoadoresRepository.java
+++ src/main/java/br/com/crux/dao/repository/TiposDoadoresRepository.java
@@ -13,7 +13,7 @@ import br.com.crux.entity.TiposDoadores;
 public interface TiposDoadoresRepository extends JpaRepository<TiposDoadores, Long>{
 
 	@Query(value = "SELECT t FROM TiposDoadores t                      "
-			+ " inner join Instituicao ins on ins.id = d.idInstituicao "
+			+ " inner join Instituicao ins on ins.id = t.idInstituicao "
 			+ " where ins.id = :idInstituicao                          "
 			+ " order by t.descricao                                   ")
 	public Optional<List<TiposDoadores>> findByIdInstituicao(Long idInstituicao);
