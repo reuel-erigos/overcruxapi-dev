f32e6848e5a2d9acf793b30304cbb9df1004c8a4
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-09-10 15:01:50 -0300

    Implementação reembolsos demanda 2020-08-09 tela de movimentações

---
+++ src/main/java/br/com/crux/dao/repository/RateiosPagamentosRepository.java
@@ -0,0 +1,21 @@
+package br.com.crux.dao.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.crux.entity.RateiosPagamentos;
+
+@Repository
+public interface RateiosPagamentosRepository extends JpaRepository<RateiosPagamentos, Long> {
+
+	@Query(value = "select distinct rp from RateiosPagamentos pr               "
+			+ " inner join PagamentosFatura pf on pf.id = pr.idPagamentoFatura "
+			+ " inner join Fatura f on f.id = pf.idFatura                      "
+			+ " where f.idMovimentacao = ?1                                    ")
+	public Optional<List<RateiosPagamentos>> findByIdMovimento(Long idMovimento);
+
+}
