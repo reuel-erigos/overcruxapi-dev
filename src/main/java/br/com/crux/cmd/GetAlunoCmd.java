f53cf884538bbe91cc43d8aec9c2741a1cc598cf
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-10-23 22:59:47 -0300

    Implementação para buscar alunos por filtro

--- src/main/java/br/com/crux/cmd/GetAlunoCmd.java
+++ src/main/java/br/com/crux/cmd/GetAlunoCmd.java
@@ -29,8 +29,7 @@ public class GetAlunoCmd {
 
 		idAluno              = Objects.isNull(idAluno) ? null : idAluno;
 		idPessoaFisicaMae    = Objects.isNull(idPessoaFisicaMae) ? null : idPessoaFisicaMae;
-		cpfPessoaFisicaAluno = StringUtils.isEmpty(cpfPessoaFisicaAluno) ? null : cpfPessoaFisicaAluno;
-		cpfPessoaFisicaAluno = cpfPessoaFisicaAluno.equals("00000000000") ? null : cpfPessoaFisicaAluno;
+		cpfPessoaFisicaAluno = StringUtils.isEmpty(cpfPessoaFisicaAluno) || cpfPessoaFisicaAluno.equals("00000000000") ? null : cpfPessoaFisicaAluno;
 
 		entitys = repository.findByFilter(idInstituicao, idAluno, idPessoaFisicaMae, cpfPessoaFisicaAluno);
 		if (entitys.isPresent()) {
