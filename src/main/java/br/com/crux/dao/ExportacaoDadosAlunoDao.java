4f85747b8f9347c45909006561abc84dfa25d454
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-11-04 01:18:37 -0300

    Implementação da DAO que retorna os registros para exprotar dos alunos

--- src/main/java/br/com/crux/dao/ExportacaoDadosAlunoDao.java
+++ src/main/java/br/com/crux/dao/ExportacaoDadosAlunoDao.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import br.com.crux.dao.base.BaseDao;
 import br.com.crux.dao.dto.ExportacaoDadosAlunoDTO;
+import br.com.crux.infra.util.Java8DateUtil;
 
 @Component
 public class ExportacaoDadosAlunoDao extends BaseDao{
@@ -21,16 +22,58 @@ public class ExportacaoDadosAlunoDao extends BaseDao{
 														        LocalDate dataInicioInstituicao, LocalDate dataFimInstituicao, Long idInstituicao){
 		StringBuilder sql = new StringBuilder();
 		
-		sql.append(" select a.id_aluno,                                                           ");
-		sql.append("        pf.nm_pessoa_fisica                                                   ");
-		sql.append("  from alunos a                                                               ");
-		sql.append(" inner join pessoas_fisicas pf on a.id_pessoa_fisica = pf.id_pessoa_fisica    ");
-		sql.append(" where 1=1                                                                    ");
-		sql.append("  and pf.id_instituicao = :idInstituicao                                      ");
+		sql.append(" select p.id_pessoa_fisica,                                                                             ");
+		sql.append("        a.nr_matricula_aluno,                                                                           ");
+		sql.append("        p.nm_pessoa_fisica,                                                                             ");
+		sql.append("        p.dt_nascimento,                                                                                ");
+		sql.append("        p.nm_mae,                                                                                       ");
+        sql.append("        p.nr_cpf,                                                                                       ");
+        sql.append("        p.nr_nis,                                                                                       ");
+        sql.append("        p.ds_email,                                                                                     ");
+        sql.append("        p.nr_fone_celular,                                                                              ");
+        sql.append("        p.nr_telefone_residencial,                                                                      ");
+        sql.append("        p.nr_telefone_comercial,                                                                        ");
+        sql.append("        u.nm_unidade,                                                                                   ");
+        sql.append("        a.dt_entrada,                                                                                   ");
+        sql.append("        a.dt_desligamento                                                                               ");
+		sql.append("    from pessoas_fisicas p                                                                              ");
+		sql.append("      inner join alunos a on p.id_pessoa_fisica = a.id_pessoa_fisica                                    ");
+		sql.append("      left join pessoas_fisicas pfm on pfm.id_pessoa_fisica = a.id_pessoa_fisica                        ");
+		sql.append("      left join pessoas_fisicas pfp on pfp.id_pessoa_fisica = a.id_pessoa_fisica                        ");
+		sql.append("      left join programas poa on poa.id_programa = a.id_programa                                        ");
+		sql.append("      left join projetos pra on pra.id_projeto = a.id_projeto                                           ");
+		sql.append("      left join alunos_turma at on at.id_aluno = a.id_aluno                                             ");
+		sql.append("      left join turmas t on t.id_turma = at.id_turma                                                    ");
+		sql.append("      left join programas po on t.id_programa = po.id_programa                                          ");
+		sql.append("      left join projetos pr on t.id_projeto = pr.id_projeto                                             ");
+		sql.append("      left join graus_instrucao gi on gi.id_grau_instrucao = p.graus_inscrucao_pf                       ");
+		sql.append("      left join unidades u on u.id_unidade = a.id_unidade                                               ");
+		sql.append(" WHERE 1 = 1                                                                                            ");
+		sql.append("  and p.id_instituicao = :idInstituicao                                                                 ");
+		sql.append("  and (:p_nr_cpf is null or :p_nr_cpf = p.nr_cpf)                                                       ");
+		sql.append("  and (:p_dt_entrada is null or :p_dt_entrada >= p.dt_entrada)                                           ");
+		sql.append("  and (:p_dt_desligamento is null or :p_dt_desligamento <= p.dt_desligamento)                            ");
+		sql.append("  and (:p_id_pessoa_fisica is null or :p_id_pessoa_fisica = p.id_pessoa_fisica)                         ");
+		sql.append("  and (:p_id_pessoa_fisica_mae is null or :p_id_pessoa_fisica_mae = pfm.id_pessoa_fisica)               ");
+		sql.append("  and (:p_id_pessoa_fisica_pai is null or :p_id_pessoa_fisica_mae = pfp.id_pessoa_fisica)               ");
+		sql.append("  and (:p_id_programa is null or :p_id_programa = poa.id_programa or :p_id_programa = po.id_programa)   ");
+		sql.append("  and (:p_id_projeto is null or :p_id_projeto = poa.id_projeto or :p_id_projeto = po.id_projeto)        ");
+		sql.append("  and (:p_id_unidade is null or :p_id_unidade = u.id_unidade)                                           ");
+		sql.append(" order by p.nm_pessoa_fisica, p.nm_mae                                                                  ");
+		
 		
 		Query query = em.createNativeQuery(sql.toString());
 		query.setParameter("idInstituicao", idInstituicao);
-		
+		query.setParameter("p_nr_cpf", cpf);
+		query.setParameter("p_dt_entrada", Java8DateUtil.getLocalDateFormater(dataInicioInstituicao));
+		query.setParameter("p_dt_desligamento", Java8DateUtil.getLocalDateFormater(dataFimInstituicao));
+		query.setParameter("p_id_pessoa_fisica", idBeneficiario);
+		query.setParameter("p_id_pessoa_fisica_mae", idMae);
+		query.setParameter("p_id_pessoa_fisica_pai", idPai);
+		query.setParameter("p_id_programa", idPrograma);
+		query.setParameter("p_id_projeto", idProjeto);
+		query.setParameter("p_id_unidade", idUnidade);
+
 		
 		@SuppressWarnings("unchecked")
 		List<Object[]> values = query.getResultList();
@@ -38,24 +81,10 @@ public class ExportacaoDadosAlunoDao extends BaseDao{
 		List<ExportacaoDadosAlunoDTO> retorno = new ArrayList<ExportacaoDadosAlunoDTO>();
 		values.stream().forEach( colunas -> retorno.add(new ExportacaoDadosAlunoDTO(colunas)));
 		
+		return Optional.ofNullable(retorno);		
+	}
+	
+	
 
-		//Dados mockados
-		retorno.addAll(mockDados());
-		
-		return Optional.ofNullable(retorno);
-		
-	}
-	
-	
-	private List<ExportacaoDadosAlunoDTO> mockDados() {
-		List<ExportacaoDadosAlunoDTO> lista = new ArrayList<ExportacaoDadosAlunoDTO>();
-		for (int i = 0; i < 11; i++) {
-			ExportacaoDadosAlunoDTO d1 = new ExportacaoDadosAlunoDTO();
-			d1.setId(Long.valueOf(i));
-			d1.setNome("Fulano de souza " + i);
-			lista.add(d1);
-		}
-		return lista;
-	}
 	
 }
--- src/main/java/br/com/crux/to/ExportacaoDadosAlunoTO.java
+++ src/main/java/br/com/crux/to/ExportacaoDadosAlunoTO.java
@@ -1,9 +1,24 @@
 package br.com.crux.to;
 
+import java.time.LocalDate;
+
 public class ExportacaoDadosAlunoTO {
 	
 	private Long id;
-	private String nome;
+	private String matricula;
+	private String nomeBeneficiario;
+	private LocalDate dataNascimento;
+	private String nomeMae;
+	private String nomePai;
+	private String cpf;
+	private String nis;
+	private String email;
+	private String celular;
+	private String foneResidencial;
+	private String foneComercial;
+	private String unidade;
+	private LocalDate dataEntrada;
+	private LocalDate dataDesligamento;
 	
 	public ExportacaoDadosAlunoTO() {
 	}
@@ -16,12 +31,118 @@ public class ExportacaoDadosAlunoTO {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getMatricula() {
+		return matricula;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
 	}
+
+	public String getNomeBeneficiario() {
+		return nomeBeneficiario;
+	}
+
+	public void setNomeBeneficiario(String nomeBeneficiario) {
+		this.nomeBeneficiario = nomeBeneficiario;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNis() {
+		return nis;
+	}
+
+	public void setNis(String nis) {
+		this.nis = nis;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getFoneResidencial() {
+		return foneResidencial;
+	}
+
+	public void setFoneResidencial(String foneResidencial) {
+		this.foneResidencial = foneResidencial;
+	}
+
+	public String getFoneComercial() {
+		return foneComercial;
+	}
+
+	public void setFoneComercial(String foneComercial) {
+		this.foneComercial = foneComercial;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public LocalDate getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(LocalDate dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public LocalDate getDataDesligamento() {
+		return dataDesligamento;
+	}
+
+	public void setDataDesligamento(LocalDate dataDesligamento) {
+		this.dataDesligamento = dataDesligamento;
+	}
+
+
 	
 }
