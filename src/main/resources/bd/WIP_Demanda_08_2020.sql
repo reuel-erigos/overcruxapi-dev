385b9e9387661d639cf81eb67cf344bf59a2f1fb
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2020-12-28 12:53:16 -0300

    ITEM 8 - TIPOS DOADORES Script

--- src/main/resources/bd/WIP_Demanda_08_2020.sql
+++ src/main/resources/bd/WIP_Demanda_08_2020.sql
@@ -5,4 +5,6 @@ INSERT INTO public.modulos (id_modulo, ds_modulo, nm_modulo, id_usuario_apl, mod
 INSERT INTO public.modulos (id_modulo, ds_modulo, nm_modulo, id_usuario_apl, modulo_pai) VALUES(nextval('sq_id_modulo'), 'Motivos Desligamentos', 'MOTIVOS_DESLIGAMENTOS', 19, 199);
 INSERT INTO public.modulos (id_modulo, ds_modulo, nm_modulo, id_usuario_apl, modulo_pai) VALUES(nextval('sq_id_modulo'), 'Público Prioritário', 'TIPOS_PUBLICOS_PRIORITARIOS', 19, 199);
 INSERT INTO public.modulos (id_modulo, ds_modulo, nm_modulo, id_usuario_apl, modulo_pai) VALUES(nextval('sq_id_modulo'), 'Tabelas de Referência - Programas e Projetos', 'TB_REFERENCIA_PROGRAMAS_PROJETOS', 3, 159);
+
+--Verifcar o id gerado no script anterior - TB_REFERENCIA_PROGRAMAS_PROJETOS.. esse id será o modulo pai nesse script, no caso de desenvolvimento o id foi 216.
 INSERT INTO public.modulos (id_modulo, ds_modulo, nm_modulo, id_usuario_apl, modulo_pai) VALUES(nextval('sq_id_modulo'), 'Tipos Doadores', 'TIPOS_DOADORES', 3, 216);
