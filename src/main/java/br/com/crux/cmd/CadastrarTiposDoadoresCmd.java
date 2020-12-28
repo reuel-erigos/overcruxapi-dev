b87faadfee1923e2923a91f31c0768245dda7055
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2020-12-28 12:02:00 -0300

    ITEM 8 - TIPOS DOADORES

---
+++ src/main/java/br/com/crux/cmd/CadastrarTiposDoadoresCmd.java
@@ -0,0 +1,29 @@
+package br.com.crux.cmd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.crux.builder.TiposDoadoresTOBuilder;
+import br.com.crux.dao.repository.TiposDoadoresRepository;
+import br.com.crux.entity.TiposDoadores;
+import br.com.crux.rule.CamposObrigatoriosTiposDoadoresRule;
+import br.com.crux.to.TiposDoadoresTO;
+
+@Component
+public class CadastrarTiposDoadoresCmd {
+	
+	@Autowired private TiposDoadoresRepository repository;
+	@Autowired private TiposDoadoresTOBuilder toBuilder;
+	@Autowired private CamposObrigatoriosTiposDoadoresRule camposObrigatoriosRule;
+	
+	
+	public void cadastrar(TiposDoadoresTO to) {
+		
+		camposObrigatoriosRule.verificar(to);
+		
+		TiposDoadores entity = toBuilder.build(to);
+		
+		repository.save(entity);
+		
+	}
+}
