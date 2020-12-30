01e6d55966544bf4ee78ba241b447e142ddff513
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-12-29 21:21:20 -0300

    Relatorios gestão pessoal

--- src/main/java/br/com/crux/dao/dto/ColaboradoresGestaoPessoalDTO.java
+++ src/main/java/br/com/crux/dao/dto/ColaboradoresGestaoPessoalDTO.java
@@ -1,27 +1,17 @@
 package br.com.crux.dao.dto;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.time.LocalDate;
 
 public class ColaboradoresGestaoPessoalDTO {
 	
 	private Long idPessoaFisica;
-	private Long idAluno;
-	private String matricula;
-	private String nomeBeneficiario;
-	private LocalDate dataNascimento;
-	private String nomeMae;
-	private String nomePai;
-	private String cpf;
-	private String nis;
+	private Long idColaborador;
+	private String nomeCobaborador;
 	private String email;
-	private String celular;
-	private String foneResidencial;
-	private String foneComercial;
 	private String unidade;
-	private LocalDate dataEntrada;
-	private LocalDate dataDesligamento;
+	private String departamento;
+	private String cargo;
+	private String funcao;
 	
 	
 	public ColaboradoresGestaoPessoalDTO() {
@@ -29,93 +19,37 @@ public class ColaboradoresGestaoPessoalDTO {
 	
 	public ColaboradoresGestaoPessoalDTO(Object[] colunas) {
 		this.idPessoaFisica      = (colunas[0] != null)? ((BigDecimal)colunas[0]).longValue() : null;
-		this.idAluno             = (colunas[1] != null)? ((BigDecimal)colunas[1]).longValue() : null;
-		this.matricula           = (String) colunas[2];
-		this.nomeBeneficiario    = (String) colunas[3];
-		this.dataNascimento      = (colunas[4] != null)? ((Timestamp)colunas[4]).toLocalDateTime().toLocalDate() : null;
-		this.nomeMae             = (String) colunas[5];
-		this.nomePai             = (String) colunas[6];
-		this.cpf                 = (String) colunas[7];
-		this.nis                 = (String) colunas[8];
-		this.email               = (String) colunas[9];
-		this.celular             = (String) colunas[10];
-		this.foneResidencial     = (String) colunas[11];
-		this.foneComercial       = (String) colunas[12];
-		this.unidade             = (String) colunas[13];
-		this.dataEntrada         = (colunas[14] != null)? ((Timestamp)colunas[14]).toLocalDateTime().toLocalDate() : null;
-		this.dataDesligamento    = (colunas[15] != null)? ((Timestamp)colunas[15]).toLocalDateTime().toLocalDate() : null;
+		this.idColaborador       = (colunas[1] != null)? ((BigDecimal)colunas[1]).longValue() : null;
+		this.nomeCobaborador     = (colunas[2] != null) ? (String) colunas[2] : ""; 
+		this.email               = (colunas[3] != null) ? (String) colunas[3] : ""; 
+		this.unidade             = (colunas[4] != null) ? (String) colunas[4] : ""; 
+		this.departamento        = (colunas[5] != null) ? (String) colunas[5] : ""; 
+		this.cargo               = (colunas[6] != null) ? (String) colunas[6] : ""; 
+		this.funcao              = (colunas[7] != null) ? (String) colunas[7] : ""; 
 	}
 
 	public Long getIdPessoaFisica() {
 		return idPessoaFisica;
 	}
 
-	public void setIdPessoaFisica(Long id) {
-		this.idPessoaFisica = id;
+	public void setIdPessoaFisica(Long idPessoaFisica) {
+		this.idPessoaFisica = idPessoaFisica;
 	}
 
-	public Long getIdAluno() {
-		return idAluno;
+	public Long getIdColaborador() {
+		return idColaborador;
 	}
 
-	public void setIdAluno(Long idAluno) {
-		this.idAluno = idAluno;
+	public void setIdColaborador(Long idColaborador) {
+		this.idColaborador = idColaborador;
 	}
 
-	public String getMatricula() {
-		return matricula;
+	public String getNomeCobaborador() {
+		return nomeCobaborador;
 	}
 
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
-
-	public String getNomeBeneficiario() {
-		return nomeBeneficiario;
-	}
-
-	public void setNomeBeneficiario(String nomeBeneficiario) {
-		this.nomeBeneficiario = nomeBeneficiario;
-	}
-
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(LocalDate dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getNomeMae() {
-		return nomeMae;
-	}
-
-	public void setNomeMae(String nomeMae) {
-		this.nomeMae = nomeMae;
-	}
-
-	public String getNomePai() {
-		return nomePai;
-	}
-
-	public void setNomePai(String nomePai) {
-		this.nomePai = nomePai;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getNis() {
-		return nis;
-	}
-
-	public void setNis(String nis) {
-		this.nis = nis;
+	public void setNomeCobaborador(String nomeCobaborador) {
+		this.nomeCobaborador = nomeCobaborador;
 	}
 
 	public String getEmail() {
@@ -126,30 +60,6 @@ public class ColaboradoresGestaoPessoalDTO {
 		this.email = email;
 	}
 
-	public String getCelular() {
-		return celular;
-	}
-
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
-
-	public String getFoneResidencial() {
-		return foneResidencial;
-	}
-
-	public void setFoneResidencial(String foneResidencial) {
-		this.foneResidencial = foneResidencial;
-	}
-
-	public String getFoneComercial() {
-		return foneComercial;
-	}
-
-	public void setFoneComercial(String foneComercial) {
-		this.foneComercial = foneComercial;
-	}
-
 	public String getUnidade() {
 		return unidade;
 	}
@@ -158,21 +68,30 @@ public class ColaboradoresGestaoPessoalDTO {
 		this.unidade = unidade;
 	}
 
-	public LocalDate getDataEntrada() {
-		return dataEntrada;
+	public String getDepartamento() {
+		return departamento;
 	}
 
-	public void setDataEntrada(LocalDate dataEntrada) {
-		this.dataEntrada = dataEntrada;
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
 	}
 
-	public LocalDate getDataDesligamento() {
-		return dataDesligamento;
+	public String getCargo() {
+		return cargo;
 	}
 
-	public void setDataDesligamento(LocalDate dataDesligamento) {
-		this.dataDesligamento = dataDesligamento;
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
 	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
 
 	
 }
