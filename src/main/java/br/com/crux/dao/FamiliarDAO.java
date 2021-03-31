3d8af86618086b38ab122926cf41aca7ec1a8741
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2021-03-30 23:23:07 -0300

    Ajuste ao retornar os dados do familiar na combo-pesquisavel

--- src/main/java/br/com/crux/dao/FamiliarDAO.java
+++ src/main/java/br/com/crux/dao/FamiliarDAO.java
@@ -17,16 +17,18 @@ public class FamiliarDAO extends BaseDao{
 	public List<ComboFamiliarDTO> getAllComboByInstituicao(Long idInstituicao) {
 		StringBuilder sql = new StringBuilder();
 		
-		sql.append(" select f.id_familiar,                                                          ");
-		sql.append("        upper(pf.nm_pessoa_fisica)                                              ");
-		sql.append("   from familiares f                                                            ");
-		sql.append(" inner join pessoas_fisicas pf on pf.id_pessoa_fisica = f.id_pessoa_fisica      ");
-		sql.append(" inner join alunos a on a.id_aluno = f.id_aluno                                 ");
-		sql.append(" inner join unidades u on u.id_unidade = a.id_unidade                           ");
-		sql.append(" inner join instituicoes inst on inst.id_instituicao = u.id_instituicao         ");
-		sql.append(" where 1=1                                                                      ");
-		sql.append("   and inst.id_instituicao = :idInstituicao                                     ");
-		sql.append("  order by pf.nm_pessoa_fisica                                                  ");
+		sql.append(" select f.id_familiar,                                                                            ");
+		sql.append("        upper(pf.nm_pessoa_fisica),                                                               ");
+		sql.append("        'RESPONSÁVEL:' || pf.nm_pessoa_fisica || ' - BENEFICIÁRIO: ' || pa.nm_pessoa_fisica       ");
+		sql.append("   from familiares f                                                                              ");
+		sql.append(" inner join pessoas_fisicas pf on pf.id_pessoa_fisica = f.id_pessoa_fisica                        ");
+		sql.append(" inner join alunos a on a.id_aluno = f.id_aluno                                                   ");
+		sql.append(" inner join pessoas_fisicas pa on pa.id_pessoa_fisica = a.id_pessoa_fisica                        ");
+		sql.append(" inner join unidades u on u.id_unidade = a.id_unidade                                             ");
+		sql.append(" inner join instituicoes inst on inst.id_instituicao = u.id_instituicao                           ");
+		sql.append(" where 1=1                                                                                        ");
+		sql.append("   and inst.id_instituicao = :idInstituicao                                                       ");
+		sql.append("  order by pf.nm_pessoa_fisica                                                                    ");
 		
 		Query query = em.createNativeQuery(sql.toString());
 		query.setParameter("idInstituicao", idInstituicao);
--- src/main/java/br/com/crux/dao/dto/ComboFamiliarDTO.java
+++ src/main/java/br/com/crux/dao/dto/ComboFamiliarDTO.java
@@ -6,13 +6,16 @@ public class ComboFamiliarDTO {
 	
 	private Long id;
 	private String nome;
+	private String nomeFamiliarAndNomeAluno;
+	
 	
 	public ComboFamiliarDTO() {
 	}
 	
 	public ComboFamiliarDTO(Object[] colunas) {
-		this.id      = (colunas[0] != null)? ((BigDecimal)colunas[0]).longValue() : null;
-		this.nome    = (String) colunas[1];
+		this.id                          = (colunas[0] != null)? ((BigDecimal)colunas[0]).longValue() : null;
+		this.nome                        = (String) colunas[1];
+		this.nomeFamiliarAndNomeAluno    = (String) colunas[2];
 	}
 
 	public Long getId() {
@@ -31,5 +34,12 @@ public class ComboFamiliarDTO {
 		this.nome = nome;
 	}
 
+	public String getNomeFamiliarAndNomeAluno() {
+		return nomeFamiliarAndNomeAluno;
+	}
+
+	public void setNomeFamiliarAndNomeAluno(String nomeFamiliarAndNomeAluno) {
+		this.nomeFamiliarAndNomeAluno = nomeFamiliarAndNomeAluno;
+	}
 	
 }
