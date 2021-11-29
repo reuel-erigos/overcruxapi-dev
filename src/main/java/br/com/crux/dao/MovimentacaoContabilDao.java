b2fa54909c259c8de8fb077eeb1deead31ad1185
reuel-erigos <reuel@erigos.com.br>
2021-11-29 14:57:32 -0300

    alterada consulta do relatório de movimentacoes contabieis para contabilizar saldos de categorias sinteticas

--- src/main/java/br/com/crux/dao/MovimentacaoContabilDao.java
+++ src/main/java/br/com/crux/dao/MovimentacaoContabilDao.java
@@ -126,79 +126,103 @@ public class MovimentacaoContabilDao extends BaseDao{
 		StringBuilder sql = new StringBuilder();
 		
 		
-		sql.append("	SELECT Case coalesce(po.id_programa, 0)                 		                                          			");
-		sql.append("        	when 0 then pj.nm_projeto               		                                                  			");
-		sql.append("	       else po.nm_programa              		                                                          			");
-		sql.append("	      end as projeto_programa, 			                                                                   			");
-		sql.append("	      m.nr_documento,                                                                             					");
-		sql.append("	      m.dt_documento,                                                                             					");
-		sql.append("	      cm.ds_categoria_movimentacao,                                                         		      			");
-		sql.append("	      cm.dt_movimentacao,                                                          			               			");
+		sql.append("SELECT * from (		                 		                                          						");
+		sql.append("	SELECT Case coalesce(po.id_programa, 0)                 		                                        ");
+		sql.append("        	when 0 then pj.nm_projeto               		                                                ");
+		sql.append("	       else po.nm_programa              		                                                        ");
+		sql.append("	      end as projeto_programa, 			                                                                ");
+		sql.append("	      m.nr_documento,                                                                             		");
+		sql.append("	      m.dt_documento,                                                                             		");
+		sql.append("	      cm.ds_categoria_movimentacao,                                                         		    ");
+		sql.append("	      cm.dt_movimentacao,                                                          			            ");
+		sql.append("	      (cm.vl_categoria_movimentacao * -1) as vl_categoria_movimentacao, 								");
+		sql.append("	      pcd.plano_conta conta_destino,                            					                    ");
+		sql.append("	      pco.plano_conta conta_origem,                                                 					");
+		sql.append("	      m.id_movimentacao,                                                                          		");
+		sql.append("	      cm.id_categoria_origem,                                                                     		");
+		sql.append("	      cm.id_categoria_destino,                                                                    		");
+		sql.append("	      cm.id_programa,                                                                             		");
+		sql.append("	      cm.id_projeto                                                                               		");
+		sql.append("     FROM movimentacoes m                                                                             		");
+		sql.append("       inner join categorias_movimentacoes cm on cm.id_movimentacao = m.id_movimentacao               		");
+		sql.append("       inner join vw_planos_contas pco ON pco.id_categoria = cm.id_categoria_origem                   		");
+		sql.append("       inner join vw_planos_contas pcd ON pcd.id_categoria = cm.id_categoria_destino                  		");
+		sql.append("       left join projetos pj on pj.id_projeto = cm.id_projeto                                         		");
+		sql.append("      left join programas po on po.id_programa = cm.id_programa                                       		");
+		sql.append(" where 1 = 1                                                                                          		");
   		if(Objects.nonNull(idCategoria)) {
- 			sql.append("	  fn_retorna_valor_categoria_contabil(cm.id_categoria_movimentacao, coalesce(:p_id_categoria, 0)) as vl_categoria_movimentacao, 	");
-  		}	
-        else {
-  			sql.append("	  cm.vl_categoria_movimentacao, 	");
-  		}                                           
-		sql.append("	      pcd.plano_conta conta_destino,                            					                                ");
-		sql.append("	      pco.plano_conta conta_origem,                                                 					            ");
-		sql.append("	      m.id_movimentacao,                                                                          					");
-		sql.append("	      cm.id_categoria_origem,                                                                     					");
-		sql.append("	      cm.id_categoria_destino,                                                                    					");
-		sql.append("	      cm.id_programa,                                                                             					");
-		sql.append("	      cm.id_projeto                                                                               					");
-		sql.append("     FROM movimentacoes m                                                                             					");
-		sql.append("       inner join categorias_movimentacoes cm on cm.id_movimentacao = m.id_movimentacao               					");
-		sql.append("       inner join vw_planos_contas pco ON pco.id_categoria = cm.id_categoria_origem                   					");
-		sql.append("       inner join vw_planos_contas pcd ON pcd.id_categoria = cm.id_categoria_destino                  					");
-		sql.append("       left join projetos pj on pj.id_projeto = cm.id_projeto                                         					");
-		sql.append("      left join programas po on po.id_programa = cm.id_programa                                       					");
-		sql.append(" where 1 = 1                                                                                          					");
-	
+  			sql.append(" and cm.id_categoria_origem in (WITH RECURSIVE arvore AS (                                             ");
+            sql.append("         SELECT DISTINCT cc.id_categoria                                                                ");
+            sql.append("           FROM categorias_contabeis cc                                                                 ");
+            sql.append("          WHERE cc.id_categoria = :p_id_categoria                                                       ");
+            sql.append("        UNION ALL                                                                                       ");
+            sql.append("         SELECT DISTINCT ci.id_categoria                                                                ");
+            sql.append("           FROM categorias_contabeis ci                                                                 ");
+            sql.append("             JOIN arvore arvore_1 ON ci.id_categoria_superior = arvore_1.id_categoria                   ");
+            sql.append("        )                                                                                               ");
+            sql.append("       SELECT arvore.id_categoria                                                                       ");
+            sql.append("         FROM arvore)                                                                                   ");
+		}                                           
+		sql.append(" union                                                                                            			");
+
+		sql.append("	SELECT Case coalesce(po.id_programa, 0)                 		                                        ");
+		sql.append("        	when 0 then pj.nm_projeto               		                                                ");
+		sql.append("	       else po.nm_programa              		                                                        ");
+		sql.append("	      end as projeto_programa, 			                                                                ");
+		sql.append("	      m.nr_documento,                                                                             		");
+		sql.append("	      m.dt_documento,                                                                             		");
+		sql.append("	      cm.ds_categoria_movimentacao,                                                         		    ");
+		sql.append("	      cm.dt_movimentacao,                                                          			            ");
+		sql.append("	      vl_categoria_movimentacao, 																		");
+		sql.append("	      pcd.plano_conta conta_destino,                            					                    ");
+		sql.append("	      pco.plano_conta conta_origem,                                                 					");
+		sql.append("	      m.id_movimentacao,                                                                          		");
+		sql.append("	      cm.id_categoria_origem,                                                                     		");
+		sql.append("	      cm.id_categoria_destino,                                                                    		");
+		sql.append("	      cm.id_programa,                                                                             		");
+		sql.append("	      cm.id_projeto                                                                               		");
+		sql.append("     FROM movimentacoes m                                                                             		");
+		sql.append("       inner join categorias_movimentacoes cm on cm.id_movimentacao = m.id_movimentacao               		");
+		sql.append("       inner join vw_planos_contas pco ON pco.id_categoria = cm.id_categoria_origem                   		");
+		sql.append("       inner join vw_planos_contas pcd ON pcd.id_categoria = cm.id_categoria_destino                  		");
+		sql.append("       left join projetos pj on pj.id_projeto = cm.id_projeto                                         		");
+		sql.append("      left join programas po on po.id_programa = cm.id_programa                                       		");
+		sql.append(" where 1 = 1                                                                                          		");
+  		if(Objects.nonNull(idCategoria)) {
+  			sql.append(" and cm.id_categoria_destino in (WITH RECURSIVE arvore AS (                                             ");
+            sql.append("         SELECT DISTINCT cc.id_categoria                                                                ");
+            sql.append("           FROM categorias_contabeis cc                                                                 ");
+            sql.append("          WHERE cc.id_categoria = :p_id_categoria                                                       ");
+            sql.append("        UNION ALL                                                                                       ");
+            sql.append("         SELECT DISTINCT ci.id_categoria                                                                ");
+            sql.append("           FROM categorias_contabeis ci                                                                 ");
+            sql.append("             JOIN arvore arvore_1 ON ci.id_categoria_superior = arvore_1.id_categoria                   ");
+            sql.append("        )                                                                                               ");
+            sql.append("       SELECT arvore.id_categoria                                                                       ");
+            sql.append("         FROM arvore)                                                                                   ");
+		}                                           
+  		
+		sql.append(" ) as saida                                                                                          		");
+
+  		
 		if(Objects.nonNull(idPrograma)) {
-			sql.append("  and :p_programa = cm.id_programa ");
+			sql.append("  and :p_programa = id_programa ");
 		}
 
 		if(Objects.nonNull(idProjeto)) {
-			sql.append("  and :p_projeto = cm.id_projeto  ");
+			sql.append("  and :p_projeto = id_projeto  ");
 		}
-
-  		if(Objects.nonNull(idCategoria)) {
-  			sql.append(" and (cm.id_categoria_origem in (WITH RECURSIVE arvore AS (                                              ");
-            sql.append("         SELECT DISTINCT cc.id_categoria                                                                 ");
-            sql.append("           FROM categorias_contabeis cc                                                                  ");
-            sql.append("          WHERE cc.id_categoria = :p_id_categoria                                                        ");
-            sql.append("        UNION ALL                                                                                        ");
-            sql.append("         SELECT DISTINCT ci.id_categoria                                                                 ");
-            sql.append("           FROM categorias_contabeis ci                                                                  ");
-            sql.append("             JOIN arvore arvore_1 ON ci.id_categoria_superior = arvore_1.id_categoria                    ");
-            sql.append("        )                                                                                                ");
-            sql.append("       SELECT arvore.id_categoria                                                                        ");
-            sql.append("         FROM arvore)                                                                                    ");
-			sql.append(" or cm.id_categoria_destino in (WITH RECURSIVE arvore AS (                                               ");
-			sql.append("                         SELECT DISTINCT cc.id_categoria                                                 ");
-			sql.append("                           FROM categorias_contabeis cc                                                  ");
-			sql.append("                          WHERE cc.id_categoria = :p_id_categoria                                        ");
-			sql.append("                        UNION ALL                                                                        ");
-			sql.append("                         SELECT DISTINCT ci.id_categoria                                                 ");
-			sql.append("                           FROM categorias_contabeis ci                                                  ");
-			sql.append("                             JOIN arvore arvore_1 ON ci.id_categoria_superior = arvore_1.id_categoria    ");
-			sql.append("                        )                                                                                ");
-			sql.append("                       SELECT arvore.id_categoria                                                        ");
-			sql.append("                         FROM arvore)                                                                    ");
-			sql.append(" )                                                                                                       ");
-		}                                           
 		
 		if(Objects.nonNull(dataInicio)) {
-			sql.append("   AND DATE_TRUNC('DAY', cm.dt_movimentacao) >= DATE_TRUNC('DAY', to_date( :p_dt_inicio ,'dd/mm/yyyy') )  ");
+			sql.append("   AND DATE_TRUNC('DAY', dt_movimentacao) >= DATE_TRUNC('DAY', to_date( :p_dt_inicio ,'dd/mm/yyyy') )  ");
 		}
 		
 		if(Objects.nonNull(dataFim)) {
-			sql.append("   AND DATE_TRUNC('DAY', cm.dt_movimentacao) <= DATE_TRUNC('DAY', to_date( :p_dt_fim ,'dd/mm/yyyy') )  ");
+			sql.append("   AND DATE_TRUNC('DAY', dt_movimentacao) <= DATE_TRUNC('DAY', to_date( :p_dt_fim ,'dd/mm/yyyy') )  ");
 		}
 		
 		
-		sql.append(" order by pcd.plano_conta, pco.plano_conta, cm.dt_movimentacao ");
+		sql.append(" order by conta_destino, dt_movimentacao,  conta_origem ");
 		
 		
 		Query query = em.createNativeQuery(sql.toString());
