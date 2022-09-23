93632f068c09906423d62aa8f91e19ce21752114
reuel-erigos <reuel@erigos.com.br>
2022-09-23 12:41:07 -0300

    Alterada consulta do relatório NormativaPagamentos para mostrar valor do item no local do valor da movimentação

--- src/main/java/br/com/crux/dao/NormativaPagamentosDao.java
+++ src/main/java/br/com/crux/dao/NormativaPagamentosDao.java
@@ -26,7 +26,7 @@ public class NormativaPagamentosDao extends BaseDao{
 		sql.append("        p.nr_documento,                                                                             ");
 		sql.append("        p.cnpj_cpf,                                                                                 ");
 		sql.append("        p.dt_documento,                                                                             ");
-		sql.append("        p.vl_movimentacao,                                   ");
+		sql.append("        c.vl_total_item vl_movimentacao,                                   							");
 		sql.append("        p.nr_doc_pagamento,                                                                         ");
 		sql.append("        p.dt_pagamento,                                                                             ");
 //		sql.append("        fn_retorna_categorias_itens_movimentacoes(p.id_movimentacao, :p_tipo_rubrica ) nm_categoria,");
