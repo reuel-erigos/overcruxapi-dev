88cb1d9985f64d1440958e7c5eacb33554bd9870
kalilalves <kalilgames1@hotmail.com>
2024-07-10 18:53:44 -0300

    Reverte da alteracao de gruposAcoes para gruposAtividades

--- src/main/java/br/com/crux/entity/GrupoAcoes.java
+++ src/main/java/br/com/crux/entity/GrupoAcoes.java
@@ -22,7 +22,7 @@ import br.com.crux.infra.dao.SimNaoConverter;
 
 
 @Entity
-@Table(name="grupos_atividades")
+@Table(name="grupos_acoes")
 public class GrupoAcoes implements Serializable {
 	private static final long serialVersionUID = 1L;
 
